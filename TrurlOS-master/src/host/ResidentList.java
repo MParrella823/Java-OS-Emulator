aba497f41a91917d13e58d9a97edcea75d9fae11
Matt <parrellam617@strose.edu>
2017-10-17 15:07:17 -0400

    adding process list

--- TrurlOS-master/src/host/ResidentList.java
+++ TrurlOS-master/src/host/ResidentList.java
@@ -12,6 +12,14 @@ public class ResidentList {
     private static int pcbCounter = 0;
     private int pid = 0;
 
+    /**
+     *
+     * Will load the user program into memory via the MMU and create a new PCB for tracking purposes
+     *
+     * @param prg Array of numbers (program)
+     * @return The PID associated with this newly created process
+     */
+
 
     public int loadProcess(int[] prg){
         pcbCounter++;
@@ -27,6 +35,16 @@ public class ResidentList {
         return pid;
     }
 
+    /**
+     *
+     *
+     * This method will take a PID number and return the segment that the program is currently loaded into
+     *
+     * @param pid The PID # of the process we are trying to locate
+     * @return The segment that the process is currently occupying
+     *
+     */
+
     public int findSegment(int pid){
         int target = -1;
         for (int i = 0; i < Globals.processList.size(); i ++){
@@ -34,11 +52,18 @@ public class ResidentList {
                 target = Globals.processList.get(i).getSegment();
                 break;
             }
-
         }
         return target;
-
     }
+
+    /**
+     *
+     * This method will remove a process from the process list (ex: terminated process)
+     *
+     * @param pid The pid # of the process that is to be removed.
+     *
+     */
+
 
     public void removeProcess(int pid){
         for (int i = 0; i < Globals.processList.size(); i++){
